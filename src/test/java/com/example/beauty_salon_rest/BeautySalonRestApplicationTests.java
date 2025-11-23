98205cac6743a11bb9c785708cbb3a035227e8de
krasenamarkova <kraschoo@abv.bg>
2025-11-23 12:32:55 +0200

    Initial commit

---
+++ src/test/java/com/example/beauty_salon_rest/BeautySalonRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beauty_salon_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalonRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
